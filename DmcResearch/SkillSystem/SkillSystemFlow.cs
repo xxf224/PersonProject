2c71be86805a1c137b07c829d90ab96e7d32d11e
Brave <xxf224@gmail.com>
2018-06-06 10:37:14 +0800

    add something

--- DmcResearch/SkillSystem/SkillSystemFlow.cs
+++ DmcResearch/SkillSystem/SkillSystemFlow.cs
@@ -1,4 +1,6 @@
-一招
+"
+普通一招：
+
 SkillButtonNormal.OnPointerDown() Function
 	SkillButtonBase.OnPointerDown() Function
 	SkillButtonBase.OnClickSkillButton() Function
@@ -6,11 +8,21 @@ SkillButtonNormal.OnPointerDown() Function
 	SceneEvent_Battle_SceneToLogic_HerosSkillRequest Event
 		LogicEventHandler.OnAttackAction() Function
 		LogicEventHandler.SendAttackClickCmd() Function
-		
-
-
+		GameCommandType.UseSkill GameCommand
+		GameCommandManager.DoHandleCommand() Function
+		LogicPlayerBaseObject.HandleCommand() Function
+		LogicEntityObject.HandleCommand() Function
+		EntityStateController.HandleCommand() Function
+		CharacterIdleState.HandleCommand() Function
+		EntityBaseState.DoUseSkillCommand() Function
+		HeroSkillManager.UseSkill(skillID) Function
+		HeroSkillManager.UseSkill(EngineSkill) Function
+		EngineSkill.BeginSkill() Function
+		EngineSkill.OnStartSkill() Function (关键函数，所有相关功能都在这里开始)
 
 	BattlePage_SkillButton.OnSkillButtonPress() Function
 		SceneEvent_Battle_SceneToLogic_HerosSkillPressRequest Event
 			LogicEventHandler.OnPressedAttackAction() Function
 			LogicEventHandler.OnPressedDownAttackAction() Function
+
+"
